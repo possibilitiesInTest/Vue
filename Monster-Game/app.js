c2566e49e1fd83e59b6f5a7ad15740604ea9082b
Gourav B <pit.wire@pm.me>
2019-02-17 10:03:03 -0500

    implement attack function

--- Monster-Game/app.js
+++ Monster-Game/app.js
@@ -18,6 +18,31 @@ new Vue({
     },
     endGame: function() {
       this.gameRunning = false;
-    }
+    },
+    attack: function() {
+      var max = 10;
+      var min = 3;
+      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.monsterHealth -= damage;
+
+      if (this.monsterHealth <= 0) {
+        alert("You won!");
+        gameRunning = false;
+        return;
+      }
+
+      max = 12;
+      min = 5;
+      damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.playerHealth -= damage;
+
+      if (this.playerHealth <= 0) {
+        alert("You won!");
+        gameRunning = false;
+        return;
+      }
+    },
+    specialAttack: function() {},
+    heal: function() {}
   }
 });
