c124d095268a92aa48a55bec3669c98a47eab3f0
Gourav B <pit.wire@pm.me>
2019-02-17 10:35:34 -0500

    refactor attack function, implement checkWin

--- Monster-Game/app.js
+++ Monster-Game/app.js
@@ -6,43 +6,52 @@ new Vue({
     gameRunning: false
   },
 
-  watch: {},
-
-  computed: {},
-
   methods: {
     startGame: function() {
       this.gameRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
     },
+
     endGame: function() {
       this.gameRunning = false;
     },
+
     attack: function() {
-      var max = 10;
-      var min = 3;
-      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-      this.monsterHealth -= damage;
-
-      if (this.monsterHealth <= 0) {
-        alert("You won!");
-        gameRunning = false;
+      this.monsterHealth -= this.calculateDamage(3, 10);
+      if (this.checkWin()) {
         return;
       }
 
-      max = 12;
-      min = 5;
-      damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-      this.playerHealth -= damage;
-
-      if (this.playerHealth <= 0) {
-        alert("You won!");
-        gameRunning = false;
-        return;
-      }
+      this.playerHealth -= this.calculateDamage(5, 12);
+      this.checkWin();
     },
+
     specialAttack: function() {},
-    heal: function() {}
+
+    heal: function() {},
+
+    calculateDamage: function(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+
+    checkWin: function() {
+      if (this.monsterHealth <= 0) {
+        if (confirm("You won! New Game?")) {
+          this.startGame();
+        } else {
+          this.gameRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm("You lost! New Game?")) {
+          this.startGame();
+        } else {
+          this.gameRunning = false;
+        }
+        return true;
+      }
+      return false;
+    }
   }
 });
