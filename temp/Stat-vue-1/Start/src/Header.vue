6759acb479a807a4efe515854d7c8e50fa1870fd
Gourav B <pit.wire@pm.me>
2019-03-11 14:49:17 -0400

    server-status example 1

---
+++ temp/Stat-vue-1/Start/src/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <header>
+          <h1>Server Status</h1>
+        </header>
+      </div>
+    </div>
+    <hr>
+  </div>
+</template>
+
+<script>
+</script>
+
