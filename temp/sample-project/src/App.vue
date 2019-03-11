cb553bff2b5111a8b1d714d73ee41b930285f4b8
Gourav B <pit.wire@pm.me>
2019-03-11 14:00:27 -0400

    temp vue examples

---
+++ temp/sample-project/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <!-- <announcement></announcement> -->
+        <app-article></app-article>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ temp/sample-project/src/Article.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="article">
+    <h1>{{ title }}</h1>
+
+    <p>Published on {{ published | moment }}</p>
+    <p class="lead">{{ content }}</p>
+  </div>
+</template>
+
+
+
+
+<script>
+import moment from "moment";
+
+export default {
+  data() {
+    return {
+      title: "This is the title",
+      published: new Date(),
+      content:
+        "Obacht! Das Fernweh frickeln der altbacken Augenschmaus. Die adrett Sättigungsbeilage. Das gebeutelt Waschweib"
+    };
+  },
+
+  filters: {
+    moment: function(value) {
+      return moment(value).format("MMM Do");
+    }
+  }
+};
+</script>
