044bc2b0ac419e2cfced7a3577a09c9733dcb05a
Gourav B <pit.wire@pm.me>
2019-03-17 15:20:47 -0400

    implement activeView() on sidebar via eventBus

--- mail-app/src/Sidebar.vue
+++ mail-app/src/Sidebar.vue
@@ -5,39 +5,41 @@
 
       <div class="user-name">
         <h5>Jane Doe</h5>
-        <span class="email-address">info@codingexplained.com</span>
+        <span c lass="email-address">info@codingexplained.com</span>
       </div>
     </div>
 
-    <ul class="inbox-nav">
-      <li class="active">
-        <a href="#" @click.prevent="navigate('app-index', 'Inbox')">
-          <i class="fa fa-inbox"></i>Inbox
-          <span class="label label-danger pull-right">?</span>
-        </a>
-      </li>
+    <div>
+      <ul class="inbox-nav">
+        <li :class="{ active: activeView == 'app-inbox'}">
+          <a href="#" @click.prevent="navigate('app-index', 'Inbox')">
+            <i class="fa fa-inbox"></i>Inbox
+            <span class="label label-danger pull-right">?</span>
+          </a>
+        </li>
 
-      <li>
-        <a href="#" @click.prevent="navigate('app-sent', 'Sent')">
-          <i class="fa fa-envelope-o"></i>Sent
-          <span class="label label-default pull-right">?</span>
-        </a>
-      </li>
+        <li :class="{ active: activeView == 'app-sent'}">
+          <a href="#" @click.prevent="navigate('app-sent', 'Sent')">
+            <i class="fa fa-envelope-o"></i>Sent
+            <span class="label label-default pull-right">?</span>
+          </a>
+        </li>
 
-      <li>
-        <a href="#" @click.prevent="navigate('app-important', 'Important')">
-          <i class="fa fa-bookmark-o"></i>Important
-          <span class="label label-warning pull-right">?</span>
-        </a>
-      </li>
+        <li :class="{ active: activeView == 'app-important'}">
+          <a href="#" @click.prevent="navigate('app-important', 'Important')">
+            <i class="fa fa-bookmark-o"></i>Important
+            <span class="label label-warning pull-right">?</span>
+          </a>
+        </li>
 
-      <li>
-        <a href="#" @click.prevent="navigate('app-trash', 'Trash')">
-          <i class="fa fa-trash-o"></i>Trash
-          <span class="label label-default pull-right">?</span>
-        </a>
-      </li>
-    </ul>
+        <li :class="{ active: activeView == 'app-trash'}">
+          <a href="#" @click.prevent="navigate('app-trash', 'Trash')">
+            <i class="fa fa-trash-o"></i>Trash
+            <span class="label label-default pull-right">?</span>
+          </a>
+        </li>
+      </ul>
+    </div>
   </aside>
 </template>
 
@@ -46,6 +48,16 @@
 import { eventBus } from "./main.js";
 
 export default {
+  created() {
+    eventBus.$on("changeView", data => {
+      this.activeView = data.tag;
+    });
+  },
+  data() {
+    return {
+      activeView: data.tag
+    };
+  },
   methods: {
     navigate(newView, title) {
       eventBus.$emit("changeView", {
