2f027131eed11b8ff16443c3a4bb67e02fdd29c2
Gourav B <pit.wire@pm.me>
2019-03-17 12:37:50 -0400

    create, register, implement sidebar component

---
+++ mail-app/src/Sidebar.vue
@@ -0,0 +1,47 @@
+<template>
+  <aside class="sm-side">
+    <div class="user-head">
+      <img src="src/assets/images/profile.jpg" alt>
+
+      <div class="user-name">
+        <h5>Jane Doe</h5>
+        <span class="email-address">info@codingexplained.com</span>
+      </div>
+    </div>
+
+    <ul class="inbox-nav">
+      <li class="active">
+        <a href="#">
+          <i class="fa fa-inbox"></i>Sent
+          <span class="label label-danger pull-right">?</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="#">
+          <i class="fa fa-envelope-o"></i>Sent
+          <span class="label label-default pull-right">?</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="#">
+          <i class="fa fa-bookmark-o"></i>Sent
+          <span class="label label-warning pull-right">?</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="#">
+          <i class="fa fa-trash-o"></i>Sent
+          <span class="label label-default pull-right">?</span>
+        </a>
+      </li>
+    </ul>
+  </aside>
+</template>
+
+
+<script>
+export default {};
+</script>
