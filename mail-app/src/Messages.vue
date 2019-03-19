9f152d503c4dc35623681e2d8b9a14efc7d763eb
Gourav B <pit.wire@pm.me>
2019-03-19 07:44:32 -0400

    create messages template

--- mail-app/src/Messages.vue
+++ mail-app/src/Messages.vue
@@ -1,4 +1,27 @@
-<template></template>
+<template>
+  <table class="table table-inbox table-hover">
+    <tbody>
+      <tr>
+        <td>
+          <input type="checkbox">
+        </td>
+        <td>
+          <a href="#">
+            <i class="fa fa-star"></i>
+          </a>
+        </td>
+        <td></td>
+        <td></td>
+        <td>
+          <i></i>
+        </td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+
+  <p>No messages here yet.</p>
+</template>
 
 
 <script>
