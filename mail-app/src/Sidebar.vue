623d6bf50fbe82986bdf8a5d0862e184a204dcc6
Gourav B <pit.wire@pm.me>
2019-03-18 21:35:41 -0400

    pass :messages to sidebar in App. create computed ()s for message filters in Sidebar.

--- mail-app/src/Sidebar.vue
+++ mail-app/src/Sidebar.vue
@@ -14,28 +14,28 @@
         <li :class="{ active: activeView == 'app-inbox'}">
           <a href="#" @click.prevent="navigate('app-inbox', 'Inbox')">
             <i class="fa fa-inbox"></i>Inbox
-            <span class="label label-danger pull-right">?</span>
+            <span class="label label-danger pull-right">{{ unreadMessages.length }}</span>
           </a>
         </li>
 
         <li :class="{ active: activeView == 'app-sent'}">
           <a href="#" @click.prevent="navigate('app-sent', 'Sent')">
             <i class="fa fa-envelope-o"></i>Sent
-            <span class="label label-default pull-right">?</span>
+            <span class="label label-default pull-right">{{ sentMessages.length }}</span>
           </a>
         </li>
 
         <li :class="{ active: activeView == 'app-important'}">
           <a href="#" @click.prevent="navigate('app-important', 'Important')">
             <i class="fa fa-bookmark-o"></i>Important
-            <span class="label label-warning pull-right">?</span>
+            <span class="label label-warning pull-right">{{ importantMessages.length }}</span>
           </a>
         </li>
 
         <li :class="{ active: activeView == 'app-trash'}">
           <a href="#" @click.prevent="navigate('app-trash', 'Trash')">
             <i class="fa fa-trash-o"></i>Trash
-            <span class="label label-default pull-right">?</span>
+            <span class="label label-default pull-right">{{ trashMessages.length }}</span>
           </a>
         </li>
       </ul>
@@ -48,6 +48,12 @@
 import { eventBus } from "./main.js";
 
 export default {
+  props: {
+    messages: {
+      type: Array,
+      required: true
+    }
+  },
   created() {
     eventBus.$on("changeView", data => {
       this.activeView = data.tag;
@@ -63,6 +69,34 @@ export default {
       eventBus.$emit("changeView", {
         tag: newView,
         title: title
+      });
+    }
+  },
+  computed: {
+    unreadMessages() {
+      return this.messages.filter(function(message) {
+        return (
+          message.type == "incoming" && !message.isRead && !message.isDeleted
+        );
+      });
+    },
+    sentMessages() {
+      return this.messages.filter(function(message) {
+        return message.type == "outgoing" && !message.isDeleted;
+      });
+    },
+    importantMessages() {
+      return this.messages.filter(function(message) {
+        return (
+          message.type == "outgoing" &&
+          !message.isImportant === true &&
+          !message.isDeleted
+        );
+      });
+    },
+    trashMessages() {
+      return this.messages.filter(function(message) {
+        return message.isDeleted === true;
       });
     }
   }
