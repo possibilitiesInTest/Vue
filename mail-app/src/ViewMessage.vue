50083c100009dec52e17605d32b6289e93e7cd93
Gourav B <pit.wire@pm.me>
2019-03-20 08:02:06 -0400

    fill out VM template using v-if, v-for, {{ }}s

--- mail-app/src/ViewMessage.vue
+++ mail-app/src/ViewMessage.vue
@@ -2,20 +2,24 @@
   <div class="inbox-body">
     <p>
       <strong>Date:</strong>
+      {{ data.message.date.fromNow() }}
     </p>
     <p>
       <strong>From:</strong>
+      {{ data.message.from.name }} <{{ data.message.from.email }}>
     </p>
     <hr>
 
-    <div class="message"></div>
+    <div v-html="data.message.content" class="message"></div>
 
-    <div class="attachments">
+    <div v-if="data.message.attachments.length > 0" class="attachments">
       <h4>Attachments</h4>
 
       <ul>
-        <li>
+        <li v-for="attachement in data.message.attachements">
           <i class="fa fa-paperclip"></i>
+          {{ attachement.fileName }}
+          {{ attachement.size }}
         </li>
       </ul>
     </div>
