11e5a677e8a182acd62d72cec435478cc40be459
Gourav B <pit.wire@pm.me>
2019-03-20 18:33:24 -0400

    import messages from messages. create data prop reqs. create messages component.

--- mail-app/src/Trash.vue
+++ mail-app/src/Trash.vue
@@ -1,8 +1,22 @@
 <template>
-  <h1>Trash</h1>
+  <div class="inbox-body">
+    <app-messages></app-messages>
+  </div>
 </template>
 
 
 <script>
-export default {};
+import Messages from "./Messages.vue";
+
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    appMessages: Messages
+  }
+};
 </script>
