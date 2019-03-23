b2cee7d8dbf0f7c0a3dfe736668830f5ea614b7a
Gourav B <pit.wire@pm.me>
2019-03-23 19:48:20 -0400

    create NewQuote template

---
+++ quotes-app/src/components/NewQuote.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <form>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <label>Quote</label>
+        <textarea rows="3" class="form-control"></textarea>
+      </div>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <button class="btn btn-primary">Add Quote</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
