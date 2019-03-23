6bcf3105e003ae1f55549020ba6720841a7d563b
Gourav B <pit.wire@pm.me>
2019-03-23 17:56:39 -0400

    install node modules

--- quotes-app/src/App.vue
+++ quotes-app/src/App.vue
@@ -1,13 +1,9 @@
 <template>
-    <div class="container">
-    
-    </div>
+  <div class="container"></div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style>
