6fa9c8df0994aace9929ade1ddb87b094eddc137
Gourav B <pit.wire@pm.me>
2019-03-23 19:59:40 -0400

    bind quote to textarea with v-model. add @click to createNew() quote method, passing this.quote via emitted event

--- quotes-app/src/components/NewQuote.vue
+++ quotes-app/src/components/NewQuote.vue
@@ -3,15 +3,27 @@
     <form>
       <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
         <label>Quote</label>
-        <textarea rows="3" class="form-control"></textarea>
+        <textarea rows="3" class="form-control" v-model="quote"></textarea>
       </div>
       <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
-        <button class="btn btn-primary">Add Quote</button>
+        <button class="btn btn-primary;" @click.prevent="createNew">Add Quote</button>
       </div>
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      quote: ""
+    };
+  },
+  methods: {
+    createnew() {
+      this.$emit("quoteAdded", this.quote);
+      this.quote = "";
+    }
+  }
+};
 </script>
