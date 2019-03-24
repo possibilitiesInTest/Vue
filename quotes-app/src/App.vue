00e7b381a288b7e0f8a623c9980a9c89c3d17a6b
Gourav B <pit.wire@pm.me>
2019-03-23 20:19:38 -0400

    create DeleteInfo panel. implement @click.native deleteQuote(index) in QG.  add method to emit delete event. add eventListener to delete quote in App. from quotes array

--- quotes-app/src/App.vue
+++ quotes-app/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="container">
     <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <app-delete-info></app-delete-info>
   </div>
 </template>
 
 <script>
 import QuoteGrid from "./components/QuoteGrid.vue";
 import NewQuote from "./components/NewQuote.vue";
+import DeleteInfo from "./components/DeleteInfo.vue";
 
 export default {
   data() {
@@ -19,11 +21,15 @@ export default {
   methods: {
     newQuote(quote) {
       this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
     }
   },
   components: {
     appQuoteGrid: QuoteGrid,
-    appNewQuote: NewQuote
+    appNewQuote: NewQuote,
+    appDeleteInfo: DeleteInfo
   }
 };
 </script>
