cbec7d42c50106d566973e4e8a53ea49cf59183a
Gourav B <pit.wire@pm.me>
2019-03-27 08:19:51 -0400

    max-directives starter files

--- temp/max-forms/src/App.vue
+++ temp/max-forms/src/App.vue
@@ -1,87 +1,106 @@
 <template>
   <div class="container">
-    <form>
+    <template v-if="!submitted">
+      <form>
+        <div class="row">
+          <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <!-- Full Name (First Name + Last Name) -->
+            <app-switch v-model="dataSwitch"></app-switch>
+            <p>Switched: {{ dataSwitch }}</p>
+            <app-full-name v-model="fullName"></app-full-name>
+
+            <!-- <div class="form-group">
+              <label for="name.first">First</label>
+              <input v-model="user.name.first" type="text" id="name.first" class="form-control">
+              <label for="name.last">Last</label>
+              <input v-model="user.name.last" type="text" id="name.last" class="form-control">
+            </div>-->
+
+            <!-- eMail -->
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input v-model="user.email" type="email" id="userEmail" class="form-control">
+            </div>
+
+            <!-- Password -->
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input v-model="user.password" type="password" id="userPassword" class="form-control">
+            </div>
+
+            <!-- Store Data? Yes/No -->
+            <div class="form-group">
+              <label for="yes">Yes</label>
+              <input v-model="user.checkbox" type="radio" id="yes" value="Yes">
+              <label for="no">No</label>
+              <input v-model="user.checkbox" type="radio" id="no" value="No">
+            </div>
+
+            <div class="form-group">
+              <button calss="btn btn-primary" @click.prevent="submitted = !submitted">Submit</button>
+            </div>
+
+            <!-- Exercise 1 -->
+            <!-- Create a Signup Form where you retrieve the following Information -->
+            <!-- Full Name (First Name + Last Name) -->
+            <!-- eMail -->
+            <!-- Password -->
+            <!-- Store Data? Yes/No -->
+
+            <!-- Exercise 2 -->
+            <!-- Only display the Form if it has NOT been submitted -->
+            <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+            <!-- Exercise 3 -->
+            <!-- Edit the Example from above and create a custom "Full Name" Control -->
+            <!-- which still holds the First Name and Last Name Input Field -->
+          </div>
+        </div>
+      </form>
+      <hr>
+    </template>
+
+    <template v-else>
+      <hr>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <!-- Full Name (First Name + Last Name) -->
-
-          <div class="form-group">
-            <label for="name.first">First</label>
-            <input v-model="user.name.first" type="text" id="name.first" class="form-control">
-            <label for="name.last">Last</label>
-            <input v-model="user.name.last" type="text" id="name.last" class="form-control">
-          </div>
-
-          <!-- eMail -->
-          <div class="form-group">
-            <label for="email">Email</label>
-            <input v-model="user.email" type="email" id="userEmail" class="form-control">
-          </div>
-
-          <!-- Password -->
-          <div class="form-group">
-            <label for="password">Password</label>
-            <input v-model="user.password" type="password" id="userPassword" class="form-control">
-          </div>
-
-          <!-- Store Data? Yes/No -->
-          <div class="form-group">
-            <label for="yes">Yes</label>
-            <input v-model="user.checkbox" type="checkbox" id="yes" value="yes">
-            <label for="no">No</label>
-            <input v-model="user.checkbox" type="checkbox" id="no" value="no">
-          </div>
-
-          <!-- Exercise 1 -->
-          <!-- Create a Signup Form where you retrieve the following Information -->
-          <!-- Full Name (First Name + Last Name) -->
-          <!-- eMail -->
-          <!-- Password -->
-          <!-- Store Data? Yes/No -->
-
-          <!-- Exercise 2 -->
-          <!-- Only display the Form if it has NOT been submitted -->
-          <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
-          <!-- Exercise 3 -->
-          <!-- Edit the Example from above and create a custom "Full Name" Control -->
-          <!-- which still holds the First Name and Last Name Input Field -->
-        </div>
-      </div>
-    </form>
-    <hr>
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h4>Your Data</h4>
-          </div>
-          <div :user="user" class="panel-body">
-              <p >Full Name: {{ user.name.first }} {{ user.name.last }}</p>
-            <p >Mail: {{ user.email }} </p>
-            <p >Password: {{ user.password }}</p>
-            <p >Store in Database?:  {{ user.checkbox }}</p>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4>Your Data</h4>
             </div>
+            <div :user="user" class="panel-body">
+              <p>Full Name: {{ fullName }}</p>
+              <p>Mail: {{ user.email }}</p>
+              <p>Password: {{ user.password }}</p>
+              <p>Store in Database?: {{ user.checkbox }}</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
 <script>
+import Switch from "./Switch.vue";
+import FullName from "./FullName.vue";
+
 export default {
   data() {
     return {
+      submitted: false,
+      dataSwitch: true,
+      fullName: "Max Schwarz",
       user: {
-        name: {
-          first: '',
-          last: ''
-        },
-      email: '',
-      password: '',
-      checkbox: ''
-    }
+        email: "",
+        password: "",
+        checkbox: ""
       }
+    };
+  },
+  components: {
+    appSwitch: Switch,
+    appFullName: FullName
   }
 };
 </script>
---
+++ temp/max-forms/src/FullName.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="form-group">
+      <label>First</label>
+      <input :value="firstName" type="text" class="form-control" @input="nameChanged(true, $event)">
+      <label>Last</label>
+      <input :value="lastName" type="text" class="form-control" @input="nameChanged(false, $event)">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  methods: {
+    nameChanged(isFirst, event) {
+      let name = "";
+      if (isFirst) {
+        name = event.target.value + " " + this.lastName;
+      } else {
+        name = this.firstName + " " + event.target.value;
+      }
+      this.value = name;
+      this.$emit("input", this.value);
+    }
+  },
+  computed: {
+    firstName() {
+      return this.value.split(" ")[0];
+    },
+    lastName() {
+      return this.value.split(" ")[1];
+    }
+  }
+};
+</script>
