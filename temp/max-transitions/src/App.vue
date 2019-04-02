d56ed08128daba4f99899a94e48d16fa21b89e06
Gourav B <pit.wire@pm.me>
2019-04-02 19:05:06 -0400

    animations w/ javascript

--- temp/max-transitions/src/App.vue
+++ temp/max-transitions/src/App.vue
@@ -4,16 +4,72 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
         <hr>
+        <select v-model="alertAnimation" class="form-control">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
+        <br>
+        <hr>
+        <transition :name="alertAnimation">
+          <div class="alert alert-info" v-show="show">This is some info: fade & slide</div>
+        </transition>
+
+        <hr>
+        <br>
         <button class="btn btn-primary" @click="show = !show">Show Alert</button>
         <br>
         <br>
         <transition name="fade">
           <div class="alert alert-info" v-if="show">This is some info</div>
         </transition>
+
         <hr>
         <br>
-        <transition name="slide">
+        <transition name="slide" type="animation">
           <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
+
+        <hr>
+        <br>
+        <transition
+          name="slide"
+          type="animation"
+          appear
+          enter-class
+          enter-active-class="animated bounce"
+          leave-class
+          leave-active-class="animated shake"
+          v-if="show"
+        >
+          <div class="alert alert-info">This is some info</div>
+        </transition>
+
+        <transition :name="alertAnimation" mode="out-in">
+          <div class="alert alert-info" v-if="show" key="info">This is some info</div>
+          <div class="alert alert-warning" v-else key="warning">Thsi is some warning</div>
+        </transition>
+
+        <hr>
+        <br>
+        <!-- JS Hooks for event transitions -->
+        <button class="btn btn-primary" @click="load=!load">Load / Remove Element</button>
+        <hr>
+        <br>
+        <transition
+          @before-enter="beforeEnter"
+          @enter="enter"
+          @after-enter="afterEnter"
+          @enter-cancelled="enterCancelled"
+          @before-leave="beforeLeave"
+          @leave="leave"
+          @after-leave="afterLeave"
+          @leave-cancelled="leaveCancelled"
+          :css="false"
+        >
+          <!-- :css = false
+          tells vue.js to skip css detection-->
+
+          <div style="width: 300px; height: 100px; background-color: lightgreen" v-if="load"></div>
         </transition>
       </div>
     </div>
@@ -24,8 +80,59 @@
 export default {
   data() {
     return {
-      show: false
+      show: false,
+      load: true,
+      alertAnimation: "fade",
+      elementWidth: 100
     };
+  },
+  methods: {
+    beforeEnter(el) {
+      console.log("beforeEnter");
+      this.elementWidth = 100;
+      el.style.width = this.elementWidth + "px";
+    },
+    enter(el, done) {
+      console.log("enter");
+      let round = 1;
+      const interval = setInterval(() => {
+        el.style.width = this.elementWidth + round * 10 + "px";
+        round++;
+        if (round > 20) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
+    },
+    afterEnter(el) {
+      console.log("afterEnter");
+    },
+    enterCancelled(el) {
+      console.log("enterCancelled");
+    },
+    beforeLeave(el) {
+      console.log("beforeLeave");
+      this.elementWidth = 300;
+      el.style.width = this.elementWidth + "px";
+    },
+    leave(el, done) {
+      console.log("leave");
+      let round = 1;
+      const interval = setInterval(() => {
+        el.style.width = this.elementWidth - round * 10 + "px";
+        round++;
+        if (round > 20) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
+    },
+    afterLeave(el) {
+      console.log("afterLeave");
+    },
+    leaveCancelled(el) {
+      console.log("leaveCancelled");
+    }
   }
 };
 </script>
@@ -51,10 +158,12 @@ export default {
 }
 
 .slide-enter {
+  opacity: 0;
 }
 
 .slide-enter-active {
   animation: slide-in 1s ease-out forwards;
+  transition: opacity 0.5s;
   /* ease-out: end slower than start
     forwards: element stays in finishing position */
 }
@@ -63,6 +172,8 @@ export default {
 }
 .slide-leave-active {
   animation: slide-out 1s ease-out forwards;
+  transition: opacity 3s;
+  opacity: 0;
 }
 
 @keyframes slide-in {
