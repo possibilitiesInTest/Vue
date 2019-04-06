e074a9fe88a7c45dd139329820e965607974a400
Gourav Bhattacharyya <pit.wire@pm.me>
2019-04-06 17:32:29 -0400

    set cmp state based on @answered and @confirmed

--- temp/mini-app-transitions/src/App.vue
+++ temp/mini-app-transitions/src/App.vue
@@ -8,7 +8,7 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <component :is="mode"></component>
+                <component :is="mode" @answered="answered($event)" @confirmed="mode='app-question'"></component>
             </div>
         </div>
     </div>
@@ -16,7 +16,7 @@
 
 <script>
 import Answer from "./components/Answer.vue";
-import Question from "./components/Question.vue;"
+import Question from "./components/Question.vue";
 
 export default {
     data() {
@@ -24,9 +24,19 @@ export default {
             mode: 'app-question'
         }
     },
+    methods: {
+        answered(isCorrect) {
+            if(isCorrect){
+                this.mode = 'app-answer'
+            } else {
+                this.mode = 'app-question'
+                alert('Wrong, try again!')
+            }
+        }
+    },
     components: {
         appQuestion: Question,
-        appAnswer: Answers
+        appAnswer: Answer
     }
 }
 
