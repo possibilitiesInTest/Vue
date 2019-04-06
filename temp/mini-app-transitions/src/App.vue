091f5a63d162f623bf516a1dd6e7ad8c136203b5
Gourav Bhattacharyya <pit.wire@pm.me>
2019-04-06 17:17:06 -0400

    create dynamic cmp, register cmps

--- temp/mini-app-transitions/src/App.vue
+++ temp/mini-app-transitions/src/App.vue
@@ -8,13 +8,27 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-
+                <component :is="mode"></component>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import Answer from "./components/Answer.vue";
+import Question from "./components/Question.vue;"
+
+export default {
+    data() {
+        return {
+            mode: 'app-question'
+        }
+    },
+    components: {
+        appQuestion: Question,
+        appAnswer: Answers
+    }
+}
 
 </script>
 
