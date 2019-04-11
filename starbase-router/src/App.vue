30756c8929007517ea4a163cd2920092c19787d8
gourav b <pit.wire@pm.me>
2019-04-11 08:52:10 -0400

    starbase-router starter files

---
+++ starbase-router/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h3>Starbase Router</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
