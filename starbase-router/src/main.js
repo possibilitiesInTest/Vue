24f29a6f97c3564322a5edeb20a1dd4d206eb79a
gourav b <pit.wire@pm.me>
2019-04-11 09:14:39 -0400

    implement vue router. pass data type of route to render in Data cmp

--- starbase-router/src/main.js
+++ starbase-router/src/main.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+// import router
+
+Vue.use(VueRouter)
+// use router
+
 import App from './App.vue'
+import Data from './components/Data.vue'
+// import data to use in route
+
+const routes = [
+  { path: '/data/:type', component: Data }
+]
+// Passed to router instance created below
+
+const router = new VueRouter({
+  routes
+})
+// instantiates router, takes routes as object
 
 new Vue({
   el: '#app',
+  router,
+  // handles rendering of data in components
   render: h => h(App)
 })
