12828d644abbe3f7c8f60934692978cdd4e78454
Gourav B <pit.wire@pm.me>
2019-04-12 13:29:10 -0400

    create types filter. create types [ ], loop through types w/ v-for, add checkedTypes [ ], add v-model of checkedTypes to checkboxes, add v-show for cT to Joke cmp.

--- jokes-app/src/components/App.vue
+++ jokes-app/src/components/App.vue
@@ -4,41 +4,48 @@
     Got jokes?
     <br>
     <br>
-       <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
- <br>
- <br>
-       <button class="btn btn-primary" @click="addJoke">Add A Joke</button>
- 
+    <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
+    <br>
+    <br>
+    <button class="btn btn-primary" @click="addJoke">Add A Joke</button>
+    <br>
+    <br>
+    <span v-for="type in types">
+      <input type="checkbox" :value="type" v-model="checkedTypes" checked>
+      <label>{{ type }}</label>&nbsp;
+    </span>
     <br>
     <div class="col-md-12"></div>
     <hr>
-    <br><br>
-      <Joke 
+    <br>
+    <br>
+    <Joke
       v-for="(joke, index) in $store.state.jokes"
+      v-show="checkedTypes.includes(joke.type)"
       :joke="joke"
       :index="index"
       :key="index"
-      />
+    />
     <!-- <div>
 {{$store.state}}
-    </div> -->
-    
-
-
+    </div>-->
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-import Joke from './Joke.vue'
+import { mapActions } from "vuex";
+import Joke from "./Joke.vue";
 
 export default {
-  methods: mapActions([
-    'initJokes',
-    'addJoke'
-  ]),
+  data() {
+    return {
+      types: ["general", "knock-knock", "programming"],
+      checkedTypes: ["general", "knock-knock", "programming"]
+    };
+  },
+  methods: mapActions(["initJokes", "addJoke", "clearJokes"]),
   components: {
     Joke
   }
-}
+};
 </script>
