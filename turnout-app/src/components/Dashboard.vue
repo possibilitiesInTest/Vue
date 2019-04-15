719286224f126a171b593d1f32dd402c00c106e5
Gourav B <pit.wire@pm.me>
2019-04-15 13:46:27 -0400

    create signOut( ) in {types,mutations, actions}. setting state.user = { }

--- turnout-app/src/components/Dashboard.vue
+++ turnout-app/src/components/Dashboard.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    Dashboard Component
+    <h3>Dashboard Component</h3>
+    <button class="btn btn-danger btn-sm signout-btn" @click="signOut">Sign Out</button>
     {{$store.state}}
   </div>
 </template>
+
+<script>
+import { firebaseApp } from "../firebaseApp";
+
+export default {
+  methods: {
+    signOut() {
+      this.$store.dispatch("signOut");
+      firebaseApp.auth().signOut();
+    }
+  }
+};
+</script>
