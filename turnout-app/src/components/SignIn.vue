88f2b8e5f516c4e43674c0bb475205f1164c8c53
Gourav B <pit.wire@pm.me>
2019-04-15 12:45:59 -0400

    create signIn form, implement userAuth w/ firebaseApp

--- turnout-app/src/components/SignIn.vue
+++ turnout-app/src/components/SignIn.vue
@@ -1,3 +1,43 @@
 <template>
-  <div>Sign In Component</div>
+  <div class="from-inline">
+    <h3>Sign In</h3>
+    <div class="form-group">
+      <input type="text" placeholder="email" class="form-control" v-model="email">
+    </div>
+    <div class="form-group">
+      <input type="password" placeholder="password" class="form-control" v-model="password">
+    </div>
+    <div class="form-group">
+      <button class="btn btn-primary" @click="signIn">Sign In</button>
+    </div>
+    <br>
+    <p>{{error.message}}</p>
+    <br>
+  </div>
 </template>
+
+<script>
+import { firebaseApp } from "../firebaseApp";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: {
+        message: ""
+      }
+    };
+  },
+  methods: {
+    signIn() {
+      firebaseApp
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .catch(error => {
+          this.error = error;
+        });
+    }
+  }
+};
+</script>
