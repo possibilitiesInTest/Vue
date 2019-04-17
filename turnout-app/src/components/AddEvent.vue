953e4682701259291794cd4ccdb8ba60fc6fb0cf
Gourav B <pit.wire@pm.me>
2019-04-16 20:40:07 -0400

    use v-model to bind form data, import eventsref, create addEvent( ). import AddEvent to Dashboard

--- turnout-app/src/components/AddEvent.vue
+++ turnout-app/src/components/AddEvent.vue
@@ -4,38 +4,47 @@
     <div class="form">
       <div class="form-group">
         <label>Title</label>
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" v-model="event.title">
       </div>
       <div class="form-group">
         <label>Description</label>
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" v-nodel="event.description">
       </div>
       <div class="form-group">
         <label>Date</label>
-        <input type="date" class="form-control">
+        <input type="date" class="form-control" v-model="event.date">
       </div>
 
       <div class="form-group">
         <label>Location</label>
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" v-model="event.location">
       </div>
 
-      <div class="form-group">
-        <label>Email</label>
-        <input type="email" class="form-control">
-      </div>
-
-      <button class="btn-sm btn-primary"></button>
+      <button class="btn-sm btn-primary" @click="addEvent"></button>
     </div>
   </div>
 </template>
 
 <script>
+import { eventsref } from "../firebaseApp";
+
 export default {
   data() {
     return {
+      event: {
+        title: "",
+        description: "",
+        date: "",
+        location: "",
+        email: ""
       }
     };
+  },
+  methods: {
+    addEvent() {
+      this.event.email = this.$store.state.user.email;
+      eventsRef.push(this.event);
+    }
   }
 };
 </script>
