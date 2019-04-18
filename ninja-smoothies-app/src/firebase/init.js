41c7d041e85065082d36b0e141555ac566b698eb
Gourav B <pit.wire@pm.me>
2019-04-18 13:34:44 -0400

    init firebase.firestore. update plugins/deps.

---
+++ ninja-smoothies-app/src/firebase/init.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+import firestore from "firebase/firestore";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCsb0d5jym4B1bH6WUvgsbah_0t9AQQF-U",
+  authDomain: "ninja-smoothies-a2361.firebaseapp.com",
+  databaseURL: "https://ninja-smoothies-a2361.firebaseio.com",
+  projectId: "ninja-smoothies-a2361",
+  storageBucket: "ninja-smoothies-a2361.appspot.com",
+  messagingSenderId: "882393146420"
+};
+const firebaseApp = firebase.initializeApp(config);
+firebaseApp.firestore().settings({ timestampsInSnapshots: true });
+
+// Export firestore db
+export default firebaseApp.firestore();
