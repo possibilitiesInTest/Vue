12566baf71356df326f143cb229581e6f1b30bfe
Gourav B <pit.wire@pm.me>
2019-04-18 18:25:29 -0400

    create addIng( ). dynamicly render feedback on empty submit

--- ninja-smoothies-app/src/components/AddSmoothie.vue
+++ ninja-smoothies-app/src/components/AddSmoothie.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="add-smoothie container">
     <h2 class="center-align indigo-text">Add New Smoothie Recipie</h2>
-    <form>
+    <form @submit.prevent="AddSmoothie">
       <div class="field title">
         <label for="title">Smoothie Title</label>
         <input type="text" name="title" v-model="title">
       </div>
       <div class="field add-ingredients">
         <label for="add-ingredients">Add an ingredient</label>
-        <input type="text" name="add-ingredient">
+        <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
       </div>
       <div class="field center-align">
+        <p v-if="feedback" class="red-text"></p>
         <button class="btn pink">Add Smoothie</button>
       </div>
     </form>
@@ -22,8 +23,29 @@ export default {
   name: "AddSmoothie",
   data() {
     return {
-      title: null
+      title: null,
+      another: null,
+      ingredients: [],
+      feedback: null
     };
+  },
+  methods: {
+    addSmoothie() {
+      console.log(this.title, this.ingredients);
+    },
+    addIng() {
+      if (this.another) {
+        this.ingredients.push(this.another);
+        // pushes first ing to ingredients
+        this.another = null;
+        // sets another val back to null
+        this.feedback = null;
+        // sets feedback back to null after each entry
+      } else {
+        this.feedback = "You must enter a value to add an ingredient";
+        // provide feedback if another is empty
+      }
+    }
   }
 };
 </script>
