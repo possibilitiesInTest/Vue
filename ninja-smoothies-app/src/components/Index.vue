70830fd6ba61457a059a73d0618f78ee29ff522e
Gourav B <pit.wire@pm.me>
2019-04-18 17:52:23 -0400

    delete smoothie from fs db and local array

--- ninja-smoothies-app/src/components/Index.vue
+++ ninja-smoothies-app/src/components/Index.vue
@@ -26,9 +26,17 @@ export default {
   },
   methods: {
     deleteSmoothie(id) {
-      this.smoothies = this.smoothies.filter(smoothie => {
-        return smoothie.id != id;
-      });
+      // });
+
+      // delete doc from firestore
+      db.collection("smoothies")
+        .doc(id)
+        .delete()
+        .then(() => {
+          this.smoothies = this.smoothies.filter(smoothie => {
+            return smoothie.id != id;
+          });
+        });
     }
   },
   created() {
@@ -39,8 +47,11 @@ export default {
         snapshot.forEach(doc => {
           // console.log(doc.data()), doc.id;
           let smoothie = doc.data();
+          // each doc.data = data object in db
           smoothie.id = doc.id;
+          // set smoothie.id to id in db
           this.smoothies.push(smoothie);
+          // push smoothies from db to local array
         });
       });
   }
