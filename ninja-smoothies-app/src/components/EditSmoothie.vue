918f421760369afa5283915fccf3df88e241beac
gourav b <pit.wire@pm.me>
2019-04-19 14:47:32 -0400

    implement ediSmoothie method passing in local value

--- ninja-smoothies-app/src/components/EditSmoothie.vue
+++ ninja-smoothies-app/src/components/EditSmoothie.vue
@@ -36,6 +36,7 @@
 
 <script>
 import db from '@/firebase/init'
+import slugify from 'slugify'
 
 export default {
     name: 'EditSmoothie',
@@ -47,9 +48,30 @@ export default {
         }
     },
     methods: {
-    EditSmoothie(){
-        console.log(this.smoothie.title, this.smoothie.ingredients)
-        },
+    editSmoothie(){
+    // console.log(this.title, this.ingredients);
+    
+    if (this.smoothie.title){
+        this.feedback = null
+        // create a slug
+        this.smoothie.slug = slugify(this.smoothie.title, {
+          replacement: '-',
+          remove: /[$*_+~.()'"!\-:@']/g,
+          lower: true
+        })
+        db.collection('smoothies').doc(this.smoothie.id).update({
+          title: this.smoothie.title,
+          ingredients: this.smoothie.ingredients,
+          slug: this.smoothie.slug
+        }).then(() => {
+          this.$router.push({ name: 'Index'})
+        }).catch(err => {
+          console.log(err)
+        })
+      } else {
+        this.feedback = "You must enter a smoothie title"
+      }
+    },
     addIng() {
       if (this.another) {
         this.smoothie.ingredients.push(this.another);
