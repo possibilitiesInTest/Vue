7fc1b316b5b0bbb1f70c31f567f1f025c0bd7f0d
gourav b <pit.wire@pm.me>
2019-04-19 14:15:09 -0400

    retireve smoothie data from db in created( ). create empty data property for smoothie. render title val after created( ) using v-if.

--- ninja-smoothies-app/src/components/EditSmoothie.vue
+++ ninja-smoothies-app/src/components/EditSmoothie.vue
@@ -1,17 +1,31 @@
 <template>
-    <div class="edit-smoothie container">
-        <h2>Edit {{ this.$route.params.smoothie_slug }}</h2>
+    <div v-if="smoothie" class="edit-smoothie container">
+        <h2>Edit {{ smoothie.title }} Smoothie</h2>
     </div>
 </template>
 
 
 <script>
+import db from '@/firebase/init'
+
 export default {
     name: 'EditSmoothie',
     data() {
         return {
-
+            smoothie: null
         }
+    },
+    created() {
+        let ref = db.collection('smoothies').where('slug', '==', this.$route.params.smoothie_slug)
+        // retrieve data from db where slug property of db object == smoothie_slug in $route.params
+        ref.get().then(snapshot => {
+            snapshot.forEach(doc => {
+                this.smoothie = doc.data()
+                // set smoothie to db document
+                this.smoothie.id = doc.id
+                // set smoothie.id to doc.id retrieved from db
+            })
+        })
     }
 }
 </script>
