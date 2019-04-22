becd38c929907b1c433a8a396c81221db1a4b79b
gourav b <pit.wire@pm.me>
2019-04-22 09:36:41 -0400

    implement gmap api

--- geo-ninjas-app/src/components/layout/Navbar.vue
+++ geo-ninjas-app/src/components/layout/Navbar.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="navbar">
-        <nav class="deep-purple darken-1">
-            <div class="container">
-                <a href="" class="brand-logo left">GeoNinjas!</a>
-                <ul class="right">
-                    <li><a href="">Sign Up</a></li>
-                    <li><a href="">Login</a></li>
-                </ul>
-            </div>
-        </nav>
-    </div>
+  <div class="navbar">
+    <nav class="deep-purple darken-1">
+      <div class="container">
+        <a href="" class="brand-logo left">GeoNinjas!</a>
+        <ul class="right">
+          <li><a href="">Signup</a></li>
+          <li><a href="">Login</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
